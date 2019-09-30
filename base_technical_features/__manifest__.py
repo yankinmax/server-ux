3695b5978913fe1cd3bb13b315f0f945057ecf6d
Jim Hoefnagels <jim.hoefnagels@dynapps.be>
2019-09-30 12:21:37 +0200

    [MIG] base_technical_features: Migration to 13.0

--- base_technical_features/__manifest__.py
+++ base_technical_features/__manifest__.py
@@ -1,10 +1,9 @@
-# coding: utf-8
 # © 2016 Opener B.V. (<https://opener.am>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Technical features group",
     "summary": "Access to technical features without activating debug mode",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Usability",
     "website": "https://github.com/oca/server-ux",
     "author": "Opener B.V., Odoo Community Association (OCA)",
--- base_technical_features/models/res_users.py
+++ base_technical_features/models/res_users.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # © 2016 Opener B.V. (<https://opener.am>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import api, fields, models
@@ -10,34 +9,31 @@ class ResUsers(models.Model):
     _inherit = 'res.users'
 
     technical_features = fields.Boolean(
-        compute='get_technical_features',
-        inverse='set_technical_features')
+        compute='_compute_get_technical_features',
+        inverse='_inverse_set_technical_features')
     show_technical_features = fields.Boolean(
         string='Show field Technical Features',
-        compute='get_show_technical_features',
+        compute='_compute_get_show_technical_features',
         help=('Whether to display the technical features field in the user '
               'preferences.'))
 
-    @api.multi
     @api.depends('groups_id')
-    def get_show_technical_features(self):
+    def _compute_get_show_technical_features(self):
         """ Only display the technical features checkbox in the user
         preferences if the user has access to them """
         users = self.env.ref('base.group_no_one').users
         for user in self:
             user.show_technical_features = user in users
 
-    @api.multi
     @api.depends('groups_id')
-    def get_technical_features(self):
+    def _compute_get_technical_features(self):
         """ Map user membership to boolean field value """
         users = self.env.ref(
             'base_technical_features.group_technical_features').users
         for user in self:
             user.technical_features = user in users
 
-    @api.multi
-    def set_technical_features(self):
+    def _inverse_set_technical_features(self):
         """ Map boolean field value to group membership, but checking
         access """
         group = self.env.ref(
